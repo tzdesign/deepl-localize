aaf23c58d6d36aa0e65c3b022bfc55a66231885a
Tobias Zimmermann <info@tzdesign.de>
2024-07-14 14:16:00 +0200

    fixing locales coming in with underscore

--- src/translate/index.ts
+++ src/translate/index.ts
@@ -49,140 +49,145 @@ export default async function translate(this: Command) {
     throw new Error("Could not find source language.");
 
   for (const targetLocaleString of options.locales) {
-    const isInformal = options.informalLocales?.includes(targetLocaleString);
-    const guessedTarget = targetLocaleString === 'en' ? 'en-US' : targetLocaleString
-    const targetLocale = new Intl.Locale(guessedTarget);
-    const targetLanguage =
-      targetLanguages.find(s => s.code === targetLocale.baseName) ??
-      targetLanguages.find(s => s.code === targetLocale.language);
+    try {
+      const isInformal = options.informalLocales?.includes(targetLocaleString);
+      const guessedTarget = targetLocaleString === 'en' ? 'en-US' : targetLocaleString.replace(/_/g, "-");
+      const targetLocale = new Intl.Locale(guessedTarget);
+      const targetLanguage =
+        targetLanguages.find(s => s.code === targetLocale.baseName) ??
+        targetLanguages.find(s => s.code === targetLocale.language);
 
-    if (targetLanguage === undefined) {
-      console.error(
-        `Could not find target language for ${targetLocaleString.green
-        }.\nSee supported target languages are ${targetLanguages.map(t => t.code).join(", ").yellow
-        }`
-      );
-      continue;
-    }
-
-    const languageName = `${targetLanguage.name} ${isInformal ? "(informal)" : ""
-      }`;
-
-    console.log(`Start translation for ${languageName.green} (${targetLocaleString})`);
-
-    const filename = outputFilename(targetLocaleString, {
-      base: options.base,
-      output: options.output,
-    });
-
-    if (fs.existsSync(filename) === false) {
-      fs.writeFileSync(
-        filename,
-        JSON.stringify(
-          { locale: targetLocaleString, translations: {} },
-          null,
-          2
-        )
-      );
-    }
-
-    const targetTranslationSafe = localizeFileSchema.safeParse(
-      JSON.parse(fs.readFileSync(filename).toString())
-    );
-
-    if (targetTranslationSafe.success === false) {
-      console.error(
-        `Could not parse ${targetLocaleString}.json make sure there is no merge conflict inside the file. Will continue with next locale.`
-          .red,
-        targetTranslationSafe.error
-      );
-      continue;
-    }
-
-    let targetTranslation = targetTranslationSafe.data;
-    const progress = new cliProgress.SingleBar(
-      {},
-      cliProgress.Presets.shades_classic
-    );
-
-    const filteredTranslations = Object.entries(translations).filter(
-      ([key]) => {
-        const translation = targetTranslation.translations[key];
-
-        return (
-          translation === undefined ||
-          (typeof translation === "string" && translation.trim() === "")
+      if (targetLanguage === undefined) {
+        console.error(
+          `Could not find target language for ${targetLocaleString.green
+          }.\nSee supported target languages are ${targetLanguages.map(t => t.code).join(", ").yellow
+          }`
         );
-      }
-    );
-
-    if (filteredTranslations.length === 0) {
-      console.log(
-        `\tNo new translations for ${languageName} found. Will skip.\n`.green
-      );
-      continue;
-    }
-
-    progress.start(filteredTranslations.length, 0);
-    for (let [key, sourceText] of filteredTranslations) {
-      if (options.compiledI18n && Boolean(sourceText) === false) {
-        sourceText = key;
-      }
-      progress.increment();
-      const metaKey =
-        targetLocale.language +
-        (options.informalLocales?.includes(targetLocaleString)
-          ? "-informal"
-          : "");
-
-      if (targetTranslation.translations[key]) continue;
-      if (meta[metaKey]?.[JSON.stringify(sourceText)]) {
-        targetTranslation.translations[key] =
-          meta[metaKey][JSON.stringify(sourceText)];
         continue;
       }
 
-      meta[metaKey] = meta[metaKey] ?? {};
+      const languageName = `${targetLanguage.name} ${isInformal ? "(informal)" : ""
+        }`;
 
-      const object =
-        typeof sourceText === "string" ? { flat: sourceText } : sourceText;
+      console.log(`Start translation for ${languageName.green} (${targetLocaleString})`);
 
-      for (const [plural, source] of Object.entries(object)) {
-        const result = await translator.translateText(
-          prepareVariables(source),
-          sourceLanguage.code as deepl.SourceLanguageCode,
-          targetLanguage.code as deepl.TargetLanguageCode,
-          {
-            formality:
-              options.informalLocales?.includes(targetLocaleString) &&
-                targetLanguage.supportsFormality
-                ? "less"
-                : undefined,
-            tagHandling: "xml",
-            ignoreTags: ["x"],
-          }
+      const filename = outputFilename(targetLocaleString, {
+        base: options.base,
+        output: options.output,
+      });
+
+      if (fs.existsSync(filename) === false) {
+        fs.writeFileSync(
+          filename,
+          JSON.stringify(
+            { locale: targetLocaleString, translations: {} },
+            null,
+            2
+          )
         );
-        if (plural === "flat") {
-          targetTranslation.translations[key] = cleanVariables(result.text);
-        } else {
-          if (
-            targetTranslation.translations[key] === undefined ||
-            targetTranslation.translations[key] === ""
-          ) {
-            targetTranslation.translations[key] = {};
-          }
-          (targetTranslation.translations[key] as Record<string, string>)[
-            plural.toString()
-          ] = cleanVariables(result.text);
+      }
+
+      const targetTranslationSafe = localizeFileSchema.safeParse(
+        JSON.parse(fs.readFileSync(filename).toString())
+      );
+
+      if (targetTranslationSafe.success === false) {
+        console.error(
+          `Could not parse ${targetLocaleString}.json make sure there is no merge conflict inside the file. Will continue with next locale.`
+            .red,
+          targetTranslationSafe.error
+        );
+        continue;
+      }
+
+      let targetTranslation = targetTranslationSafe.data;
+      const progress = new cliProgress.SingleBar(
+        {},
+        cliProgress.Presets.shades_classic
+      );
+
+      const filteredTranslations = Object.entries(translations).filter(
+        ([key]) => {
+          const translation = targetTranslation.translations[key];
+
+          return (
+            translation === undefined ||
+            (typeof translation === "string" && translation.trim() === "")
+          );
+        }
+      );
+
+      if (filteredTranslations.length === 0) {
+        console.log(
+          `\tNo new translations for ${languageName} found. Will skip.\n`.green
+        );
+        continue;
+      }
+
+      progress.start(filteredTranslations.length, 0);
+      for (let [key, sourceText] of filteredTranslations) {
+        if (options.compiledI18n && Boolean(sourceText) === false) {
+          sourceText = key;
+        }
+        progress.increment();
+        const metaKey =
+          targetLocale.language +
+          (options.informalLocales?.includes(targetLocaleString)
+            ? "-informal"
+            : "");
+
+        if (targetTranslation.translations[key]) continue;
+        if (meta[metaKey]?.[JSON.stringify(sourceText)]) {
+          targetTranslation.translations[key] =
+            meta[metaKey][JSON.stringify(sourceText)];
+          continue;
         }
 
-        meta[metaKey][source] = cleanVariables(result.text);
-      }
-    }
-    progress.stop();
-    console.log("");
+        meta[metaKey] = meta[metaKey] ?? {};
 
-    fs.writeFileSync(filename, JSON.stringify(targetTranslation, null, 2));
+        const object =
+          typeof sourceText === "string" ? { flat: sourceText } : sourceText;
+
+        for (const [plural, source] of Object.entries(object)) {
+          const result = await translator.translateText(
+            prepareVariables(source),
+            sourceLanguage.code as deepl.SourceLanguageCode,
+            targetLanguage.code as deepl.TargetLanguageCode,
+            {
+              formality:
+                options.informalLocales?.includes(targetLocaleString) &&
+                  targetLanguage.supportsFormality
+                  ? "less"
+                  : undefined,
+              tagHandling: "xml",
+              ignoreTags: ["x"],
+            }
+          );
+          if (plural === "flat") {
+            targetTranslation.translations[key] = cleanVariables(result.text);
+          } else {
+            if (
+              targetTranslation.translations[key] === undefined ||
+              targetTranslation.translations[key] === ""
+            ) {
+              targetTranslation.translations[key] = {};
+            }
+            (targetTranslation.translations[key] as Record<string, string>)[
+              plural.toString()
+            ] = cleanVariables(result.text);
+          }
+
+          meta[metaKey][source] = cleanVariables(result.text);
+        }
+      }
+
+      progress.stop();
+      console.log("");
+
+      fs.writeFileSync(filename, JSON.stringify(targetTranslation, null, 2));
+    } catch (error) {
+      console.error(targetLocaleString, error);
+    }
   }
 
   writeMeta(meta, outputDir(options.base ?? "", options.output));
